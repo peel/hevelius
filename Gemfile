41e2e835de7c34e6829b80b73bbe6fbc8a302bd5
Piotr Limanowski <piotr.limanowski@schibsted.pl>
2015-03-23 16:05:29 +0100

    intial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+gem "lita"
+
+#adapter
+gem "lita-slack"
+
+#handlers
+gem "lita-google-images"
+gem "lita-karma"
---
+++ lita_config.rb
@@ -0,0 +1,33 @@
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = "Johnny Hevelius"
+  config.robot.mention_name = "hevelius"
+  config.robot.alias = "h"
+
+  # The locale code for the language to use.
+  config.robot.locale = :pl
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :info
+
+  # An array of user IDs that are considered administrators. These users
+  # the ability to add and remove other users from authorization groups.
+  # What is considered a user ID will change depending on which adapter you use.
+  # config.robot.admins = ["1", "2"]
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  config.robot.adapter = :slack
+  config.robot.admins = [""]
+  config.adapters.slack.token = "xoxb-4146560239-7bhj4u4aMMCo95SjAHt3kHNd"
+
+  ## Example: Set options for the Redis connection.
+  config.redis[:url] = ENV["REDISTOGO_URL"]
+  config.redis.port = ENV["PORT"]
+
+  ## Example: Set configuration for any loaded handlers. See the handler's
+  ## documentation for options.
+  # config.handlers.some_handler.some_config_key = "value"
+end
